f73444746cc4948ba3a123968eccf6ff19ad99cf
Malcolm Sparks <malcolm@congreve.com>
2014-04-07 10:53:35 +0100

    Releasing 0.1.2

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject cylon "0.1.2-SNAPSHOT"
+(defproject cylon "0.1.2"
   :description "An integrated security system for applications built on component"
   :url "https://github.com/juxt/cylon"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[juxt.modular/bidi "0.3.0-SNAPSHOT"]
+  :dependencies [[juxt.modular/bidi "0.3.0"]
                  [prismatic/schema "0.2.1"]
                  [ring/ring-core "1.2.2"]
                  [hiccup "1.0.5"]])
