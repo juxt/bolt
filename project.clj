50e8631db7581f64f98a6c1b5c9b857383532e15
Malcolm Sparks <malcolm@juxt.pro>
2014-05-18 13:50:35 +0100

    Readying for 0.2.0 release

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject cylon "0.2.0-SNAPSHOT"
+(defproject cylon "0.2.0"
   :description "An integrated security system for applications built on component"
   :url "https://github.com/juxt/cylon"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[juxt.modular/bidi "0.4.0-SNAPSHOT"]
-                 [juxt.modular/ring "0.4.0-SNAPSHOT"]
+  :dependencies [[juxt.modular/bidi "0.4.0"]
+                 [juxt.modular/ring "0.4.0"]
                  [prismatic/schema "0.2.1"]
                  [ring/ring-core "1.2.2"]
                  [hiccup "1.0.5"]
