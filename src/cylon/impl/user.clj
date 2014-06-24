b25549e4e97890e04c9c245248903e6da52315d8
Malcolm Sparks <malcolm@juxt.pro>
2014-06-24 02:00:44 +0100

    Add validation

--- src/cylon/impl/user.clj
+++ src/cylon/impl/user.clj
@@ -9,16 +9,18 @@
    [clojure.tools.logging :refer :all]
    [com.stuartsierra.component :as component]
    [cylon.user :refer (UserStore get-user store-user! UserDomain)]
-   [cylon.password :refer (create-hash verify-password)])
+   [cylon.password :refer (create-hash verify-password)]
+   [schema.core :as s])
   (:import
    (java.security SecureRandom)))
 
-(defrecord UserFile [f]
+(defrecord UserFile []
   component/Lifecycle
   (start [this]
-    (assoc this
-      :ref (ref (if (.exists f) (read-string (slurp f)) {}))
-      :agent (agent f)))
+    (let [f (:file this)]
+      (assoc this
+        :ref (ref (if (.exists f) (read-string (slurp f)) {}))
+        :agent (agent f))))
   (stop [this] this)
 
   UserStore
@@ -29,14 +31,20 @@
      (send-off (:agent this)
                (fn [f]
                  (spit f (with-out-str (pprint @(:ref this))))
-                 f))
+                 (:file this)))
      (get @(:ref this) uid))))
 
-(defn new-user-file [& {f :file}]
-  (let [f (io/file f)]
-    (assert (.exists (.getParentFile (.getCanonicalFile f)))
-            (format "Please create the directory structure which should contain the file: %s" f))
-    (->UserFile f)))
+(defn check-file-parent [{f :file :as opts}]
+  (assert (.exists (.getParentFile (.getCanonicalFile f)))
+          (format "Please create the directory structure which should contain the file: %s" f))
+  opts)
+
+(defn new-user-file [& {:as opts}]
+  (->> opts
+       (s/validate {:file s/Any})
+       (#(update-in % [:file] io/file))
+       check-file-parent
+       map->UserFile))
 
 (defrecord DefaultUserDomain []
   component/Lifecycle
