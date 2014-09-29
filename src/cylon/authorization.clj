87b069c5b11b193473f411e57c2ac7fece7845c3
Malcolm Sparks <malcolm@juxt.pro>
2014-09-29 16:23:09 +0100

    Add multi request authorizer

--- src/cylon/authorization.clj
+++ src/cylon/authorization.clj
@@ -12,3 +12,22 @@
    request :- Request
    requirement :- s/Any]
   (p/authorized-request? component request requirement))
+
+(defrecord MultiRequestAuthorizer [mappings]
+  RequestAuthorizer
+  (request-authorized? [this request role]
+    (when-let [header (get-in request [:headers "authorization"])]
+      (let [token-type (first (str/split (str/trim header) #"\s"))
+            dependency (get mappings token-type)]
+        (if-let [authorizer (get this dependency)]
+          (request-authorized? authorizer request role)
+          (debugf "Unrecognized token type (%s -> %s) in incoming Authorization header, with mappings as %s" token-type dependency mappings))))))
+
+(def new-multi-request-authorizer-schema
+  {:mappings {s/Str s/Keyword}})
+
+(defn new-multi-request-authorizer [& {:as opts}]
+  (->> opts
+       (merge {})
+       (s/validate new-multi-request-authorizer-schema)
+       (map->MultiRequestAuthorizer)))
