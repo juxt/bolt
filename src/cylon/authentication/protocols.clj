d691cf0ad104bdc6a186886b5658a6e7f4d3161f
Malcolm Sparks <malcolm@juxt.pro>
2014-10-14 21:29:42 +0100

    Add protocol descriptions

--- src/cylon/authentication/protocols.clj
+++ src/cylon/authentication/protocols.clj
@@ -3,13 +3,16 @@
 (ns cylon.authentication.protocols)
 
 (defprotocol RequestAuthenticator
-  ;; Determine if given credentials (found in request)
-  (authenticate [_ req]))
+  (authenticate [_ request]
+    "Return (as a map) any credentials that can be determined from the
+    given Ring request"))
 
 (extend-protocol RequestAuthenticator
   nil
-  (authenticate [_ req] nil))
+  (authenticate [_ request] nil))
 
 (defprotocol AuthenticationHandshake
-  (initiate-authentication-handshake [_ request])
+  (initiate-authentication-handshake [_ request]
+    "Return a Ring response that redirects the user-agent into an
+    interaction to establish its authenticity")
   (get-outcome [_ request]))
