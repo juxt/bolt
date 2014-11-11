a94f3de08262e26f5fff08191762183e39682d7b
Malcolm Sparks <malcolm@juxt.pro>
2014-11-11 23:17:27 +0000

    Name changes and authentication helpers

--- src/cylon/authentication.clj
+++ src/cylon/authentication.clj
@@ -19,7 +19,12 @@
    request :- Request]
   (p/authenticate component request))
 
-(defrecord DispatchingRequestAuthenticator [mappings]
+;; An authenticator that checks an authorization header. It takes a map,
+;; which maps Authorization token types to the local keywords of its
+;; dependencies that process them. Using declarations are automatically
+;; added.
+
+(defrecord AuthorizationHeaderRequestAuthenticator [mappings]
   p/RequestAuthenticator
   (authenticate [this request]
     (when-let [header (get-in request [:headers "authorization"])]
@@ -29,24 +34,42 @@
           (authenticate delegate-authenticator request)
           (debugf "Unrecognized token type (%s -> %s) in incoming Authorization header, with mappings as %s" token-type dependency mappings))))))
 
-(def new-dispatching-request-authenticator-schema
+(def new-authorization-header-request-authenticator-schema
   {:mappings {s/Str s/Keyword}})
 
-(defn new-dispatching-request-authenticator [& {:as opts}]
+(defn new-authorization-header-request-authenticator [& {:as opts}]
   (->> opts
        (merge {})
-       (s/validate new-dispatching-request-authenticator-schema)
-       (map->DispatchingRequestAuthenticator)
+       (s/validate new-authorization-header-request-authenticator-schema)
+       (map->AuthorizationHeaderRequestAuthenticator)
        (<- (using (-> opts :mappings vals vec)))))
 
-;; Utility
+;;
 
-;; TODO: It's possible that it would be useful to memoize authentication on each request
+;; Either - try multiple authenticators until one returns
+
+(defrecord EitherRequestAuthenticator []
+  p/RequestAuthenticator
+  (authenticate [this request]
+    (infof "Either: %s" (keys this))
+    (some (fn [a]
+            (when (satisfies? p/RequestAuthenticator a)
+              (let [res (authenticate a request)]
+                (when res (assoc res ::authenticator a)))))
+          (vals this))))
+
+(defn new-either-request-authenticator [& {:as opts}]
+  (->> opts
+       (merge {})
+       (map->EitherRequestAuthenticator)))
+
+;; Utility functions
+
+;; TODO: It's possible that it would be useful to memoize authentication
+;; on each request
 
 (defn get-subject-identifier [authenticator req]
   (:cylon/subject-identifier (authenticate authenticator req)))
-
-;; AuthenticationHandshake
 
 (s/defn initiate-authentication-handshake :- Response
   [component :- (s/protocol p/AuthenticationHandshake)
