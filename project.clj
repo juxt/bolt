5ba36992ced35bce9c6424fd2746576101304058
tangrammer <juanantonioruz@gmail.com>
2015-01-13 10:02:48 +0100

    Add co-dependency

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [prismatic/schema "0.2.1"]
                  [prismatic/plumbing "0.2.2"]
                  [ring/ring-core "1.2.2"]
-
+                 [malcolmsparks/co-dependency "0.1.5"]
                  [hiccup "1.0.5"]
                  [org.clojure/tools.logging "0.2.6"]
 
