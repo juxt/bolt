1f60065779431137b1f9ed1173d5e7a4c2f60713
Malcolm Sparks <malcolm@juxt.pro>
2015-06-18 09:23:15 +0100

    Removed old session protocol from JWS session

--- src/bolt/session/jwt_session.clj
+++ src/bolt/session/jwt_session.clj
@@ -21,37 +21,7 @@
 
   SessionData
   (session-data [_ request] (when-let [token (-> request cookies-request :cookies (get cookie-id) :value)]
-                              (jws/unsign token "secret")))
-
-  ;; Deprecated
-  SessionStore
-  (session
-   [_ request]
-   (throw (ex-info "Deprecated: old SessionStore protocol" {}))
-   (when-let [token (-> request cookies-request :cookies (get cookie-id) :value)]
-     (jws/unsign token "secret")))
-
-  (respond-close-session!
-   [_ request response]
-   (throw (ex-info "Deprecated: old SessionStore protocol" {}))
-   (cookies-response
-    (merge-with merge response
-                {:cookies {cookie-id {:value ""
-                                      :expires (.toGMTString (java.util.Date. 70 0 1))
-                                      :path "/"}}})))
-
-  (respond-with-new-session!
-   [_ request data response]
-   (throw (ex-info "Deprecated: old SessionStore protocol" {}))
-   (cookies-response
-    (merge-with merge response
-                {:cookies {cookie-id {:value (jws/sign data "secret")
-                                      :path "/"}}})))
-
-  (assoc-session-data!
-   [_ req m]
-   (throw (ex-info "Deprecated: old SessionStore protocol" {}))
-   (throw (ex-info "TODO: Assoc new data and resign" {}))))
+                              (jws/unsign token "secret"))))
 
 (defn new-jwt-session [& {:as opts}]
   (->> opts
