901e497a6783697491de754318de68a63c4875d2
Malcolm Sparks <malcolm@juxt.pro>
2015-07-03 07:54:21 +0100

    Formatting

--- src/bolt/session/jwt_session.clj
+++ src/bolt/session/jwt_session.clj
@@ -7,24 +7,29 @@
 
 (s/defrecord JsonWebTokenSession
     [cookie-id :- s/Str]
+
   SessionLifecycle
   (start-session!
    [_ response data]
    (cookies-response
-    (merge-with merge response
-                {:cookies {cookie-id {:value (jws/sign data "secret")
-                                      :path "/"}}})))
+    (merge-with
+     merge response
+     {:cookies {cookie-id {:value (jws/sign data "secret")
+                           :path "/"}}})))
   (stop-session!
    [_ response _]
    (cookies-response
-    (merge-with merge response
-                {:cookies {cookie-id {:value ""
-                                      :expires (.toGMTString (java.util.Date. 70 0 1))
-                                      :path "/"}}})))
+    (merge-with
+     merge response
+     {:cookies {cookie-id {:value ""
+                           :expires (.toGMTString (java.util.Date. 70 0 1))
+                           :path "/"}}})))
 
   SessionData
-  (session-data [_ request] (when-let [token (-> request cookies-request :cookies (get cookie-id) :value)]
-                              (jws/unsign token "secret"))))
+  (session-data
+   [_ request]
+   (when-let [token (-> request cookies-request :cookies (get cookie-id) :value)]
+     (jws/unsign token "secret"))))
 
 (defn new-jwt-session [& {:as opts}]
   (->> opts
