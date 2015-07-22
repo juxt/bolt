686ebabf0993f9f7742be4b1cc5db3343397033f
Malcolm Sparks <malcolm@juxt.pro>
2015-07-22 12:14:53 +0200

    Some changes to Schema type placement

--- src/bolt/user.clj
+++ src/bolt/user.clj
@@ -19,44 +19,51 @@
    user :- User]
   (p/create-user! component id user))
 
-(s/defn find-user :- (s/maybe User)
+(s/defn find-user
   [component :- (s/protocol p/UserStore)
    id :- s/Str]
+  :- (s/maybe User)
   (p/find-user component id))
 
-(s/defn update-user! :- nil
+(s/defn update-user!
   [component :- (s/protocol p/UserStore)
    id :- s/Str
    user :- User]
+  :- nil
   (p/update-user! component id user))
 
-(s/defn delete-user! :- nil
+(s/defn delete-user!
   [component :- (s/protocol p/UserStore)
    id :- s/Str]
+  :- nil
   (p/delete-user! component id))
 
-(s/defn verify-email! :- nil
+(s/defn verify-email!
   [component :- (s/protocol p/UserStore)
    email :- s/Str]
+  :- nil
   (p/verify-email! component email))
 
 ;; UserAuthenticator API
 
-(s/defn authenticate-user :- s/Any
+(s/defn authenticate-user
   [component :- (s/protocol p/UserAuthenticator)
    user :- User
    evidence :- {s/Keyword s/Str}]
+  :- s/Any
   (p/authenticate-user component user evidence))
 
 ;; UserPasswordHasher API
 
-(s/defn hash-password :- s/Str
+(s/defn hash-password
   [component :- (s/protocol p/UserPasswordHasher)
    password :- s/Str]
+  :- s/Str
   (p/hash-password component password))
 
 ;; UserStoreAdmin API
 
-(s/defn list-users :- [s/Any]
+(s/defn list-users
   [component :- (s/protocol p/UserStoreAdmin)]
+  :- [s/Any]
   (p/list-users component))
